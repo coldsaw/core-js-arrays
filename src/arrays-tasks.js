fe806c9c67e13d9294be03f8f09eb687d93ccf0a
Viktoria Sinyaeva <sinyaeva.viktoria@gmail.com>
2024-01-01 19:05:06 +0300

    feat: continue task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -271,8 +271,25 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  const array = [];
+  let depth = n;
+  function fillArr(arr) {
+    let arrCopy = arr;
+    arrCopy.length = size;
+    if (depth > 1) {
+      arrCopy.fill([]);
+      depth -= 1;
+      arrCopy = arrCopy.map((el) => fillArr(el));
+    }
+    if (depth === 1) {
+      arrCopy.fill(0);
+      depth -= 1;
+    }
+    return arrCopy;
+  }
+  fillArr(array);
+  return array;
 }
 
 /**
@@ -286,8 +303,8 @@ function createNDimensionalArray(/* n, size */) {
  *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
  *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
  */
-function flattenArray(/* nestedArray */) {
-  throw new Error('Not implemented');
+function flattenArray(nestedArray) {
+  return nestedArray.flat(Infinity);
 }
 
 /**
@@ -303,8 +320,8 @@ function flattenArray(/* nestedArray */) {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap((el) => childrenSelector(el));
 }
 
 /**
@@ -320,8 +337,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((acc, el) => acc + el[0] - el[1], 0);
 }
 
 /**
